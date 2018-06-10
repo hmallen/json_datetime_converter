ffc9ee995b9a7facd7d28ba1a312674e8a9ec0ce
hmallen <allenhm@gmail.com>
2018-06-09 22:11:08 -0500

    Removed json file path definition from __init__ and added to functions.

--- json_datetime_converter/json_datetime_converter.py
+++ json_datetime_converter/json_datetime_converter.py
@@ -3,14 +3,14 @@ import logging
 import os
 import sys
 
-#logging.basicConfig()
+logging.basicConfig()
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
 
 class JSONDatetimeConverter:
-    def __init__(self, json_file):
-        self.json_file = json_file
+    def __init__(self):
+        pass
 
 
     # Convert to and from types encodable by json module
@@ -49,7 +49,7 @@ class JSONDatetimeConverter:
             return dt_modified
 
 
-    def read_json(self):
+    def read_json(self, json_file):
         json_converted_return = {'data': None, 'status': None}
 
         conversion_list = ['heartbeat_last', 'heartbeat_timeout', 'heartbeat_delta',
@@ -58,7 +58,7 @@ class JSONDatetimeConverter:
         json_data_converted = {}
 
         try:
-            with open(self.json_file, 'r', encoding='utf-8') as file:
+            with open(json_file, 'r', encoding='utf-8') as file:
                 json_data_raw = json.loads(file.read())
 
             for data in json_data_raw:
@@ -89,7 +89,7 @@ class JSONDatetimeConverter:
             return json_converted_return
 
 
-    def write_json(self, json_data):
+    def write_json(self, json_data, json_file):
         json_converted_return = {'data': None, 'status': None}
 
         conversion_list = ['heartbeat_last', 'heartbeat_timeout', 'heartbeat_delta',
@@ -105,7 +105,7 @@ class JSONDatetimeConverter:
                 else:
                     json_data_converted[data] = json_data[data]
 
-            with open(self.json_file, 'w', encoding='utf-8') as file:
+            with open(json_file, 'w', encoding='utf-8') as file:
                 json.dump(json_data_converted, file, indent=4, sort_keys=True, ensure_ascii=False)
 
             json_converted_return['status'] = True
@@ -121,11 +121,11 @@ class JSONDatetimeConverter:
 
 
 if __name__ == '__main__':
-    test_file = 'test.json'
-
-    json_dt_converter = JSONDatetimeConverter(json_file=test_file)
+    json_dt_converter = JSONDatetimeConverter()
 
     """
+    test_file = 'test.json'
+
     json_data = json_dt_converter.read_json(test_file)
 
     print('json_data[\'data\']: ', json_data['data'])
@@ -133,7 +133,7 @@ if __name__ == '__main__':
 
     json_data['heartbeat_last'] = datetime.datetime.now()
 
-    json_data = json_dt_converter.write_json(json_data=json_data)
+    json_data = json_dt_converter.write_json(json_data=json_data, json_file=test_file)
 
     print('json_data[\'data\']: ', json_data['data'])
     print('json_data['\status\']: ', json_data['status'])
